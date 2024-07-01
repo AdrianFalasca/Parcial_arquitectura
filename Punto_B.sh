edd59d3910237782cb895df3954a2e7e23f3dcb2
Adrián <adrifalasca@hotmail.com>
2024-07-01 15:36:39 -0300

    c

---
+++ Punto_B.sh
@@ -0,0 +1,14 @@
+-------------Punto B -------------
+   74  sudo touch /usr/local/bin/FalascaAltaUser-Groups.sh
+   75  sudo ll /usr/local/bin/FalascaAltaUser-Groups.sh
+   76  ll /usr/local/bin/FalascaAltaUser-Groups.sh
+   77  chmod 757 /usr/local/bin/FalascaAltaUser-Groups.sh
+   78  sudo chmod 757 /usr/local/bin/FalascaAltaUser-Groups.sh
+   79  ll /usr/local/bin/FalascaAltaUser-Groups.sh
+   80  vim /usr/local/bin/FalascaAltaUser-Groups.sh
+   81  cat /usr/local/bin/FalascaAltaUser-Groups.sh
+   82  FalascaAltaUser-Groups.sh UTN-FRA_SO_Examenes/202406/bash_script/Lista_Usuarios.txt 
+   83  ll /work/
+   84  logout
+   85  ll /work/
+   86  su - 2P_202406_Prog1
